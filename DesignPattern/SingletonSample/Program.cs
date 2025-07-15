f9e65ce87bbe6ee65ff6ad0d178f4a3c06d93c40
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2025-07-15 17:36:41 +0700

    Add Singleton Sample

---
+++ DesignPattern/SingletonSample/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonSample;
+
+Thread process1 = new Thread(() =>
+{
+    Singleton singleton =  Singleton.GetInstance("value 1");
+    singleton.Print(singleton.InitialValue);
+});
+Thread process2 = new Thread(() =>
+{
+    Singleton singleton = Singleton.GetInstance("value 2");
+    singleton.Print(singleton.InitialValue);
+}); 
+
+process1.Start();
+process2.Start();   
+
+process1.Join();
+process2.Join();    
---
+++ DesignPattern/SingletonSample/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+    // 
+    public sealed class Singleton
+    {
+        private static Singleton? _instance = null;
+        public string InitialValue { get; private set; }
+
+        private static readonly object _lock = new object();
+        private Singleton() { }
+
+        public static Singleton GetInstance(string initialValue)
+        {
+            lock (_lock) {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                    _instance.InitialValue = initialValue;
+                }
+                return _instance;
+            }
+        
+        }
+
+        public void Print(string message)
+        {
+           Console.WriteLine(message);
+        }
+    }
+}
